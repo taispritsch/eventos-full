4dd6630252e6c8cca461568f2b45ee6f4ec897e5
tais.pritsch <tais.pritsch@universo.univates.br>
2024-07-24 13:14:59 -0300

    add testes

---
+++ tests/Feature/PerformanceUsuariosTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PerformanceUsuariosTest extends TestCase
+{
+    /** @test */
+    public function it_responds_within_a_reasonable_time()
+    {
+        $startTime = microtime(true);
+
+        $response = $this->get('/api/usuarios');
+
+        $endTime = microtime(true);
+        $duration = ($endTime - $startTime) * 1000; 
+
+        $response->assertStatus(200);
+
+        $this->assertLessThan(200, $duration, "A resposta demorou mais de 200 milissegundos.");
+    }
+}
